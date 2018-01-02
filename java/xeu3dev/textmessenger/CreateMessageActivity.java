df570c6e461b7a79a695872ce587cfaad1854e4a
xeu3 <xeu3dev@yandex.ru>
2018-01-02 16:10:12 +0300

    commit_1

---
+++ java/xeu3dev/textmessenger/CreateMessageActivity.java
@@ -0,0 +1,25 @@
+package xeu3dev.textmessenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+    public void onSendMessage (View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,messageText);
+        startActivity (intent);
+
+    }
+}
